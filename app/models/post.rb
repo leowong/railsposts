d851cd5d1aea0f9724d1ff35473bbbfa565c9fa6
Leo Wong <gnowoel@gmail.com>
2010-01-15 02:52:23 +0800

    fixing a typo in sql conditions

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   @@per_page = 50
 
   def self.search(search, page)
-    conditions = ["news_feeds.source == ?",  search] if search
+    conditions = ["news_feeds.source = ?",  search] if search
     paginate :page => page,
              :joins => :news_feed,
              :select => "posts.*, news_feeds.source as source",
