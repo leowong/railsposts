0189107a3ff05deb88ff80dbe64adad602dec33c
Leo Wong <gnowoel@gmail.com>
2010-01-18 03:41:05 +0800

    putting sessions in the database

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -19,4 +19,8 @@ module LayoutHelper
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
+
+  def tag_line
+    "feeds collection for Rails developers"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100117110636) do
+ActiveRecord::Schema.define(:version => 20100117193352) do
 
   create_table "news_feeds", :force => true do |t|
     t.string   "url"
@@ -32,5 +32,15 @@ ActiveRecord::Schema.define(:version => 20100117110636) do
     t.integer  "points",       :default => 0,   :null => false
     t.float    "weight",       :default => 0.0, :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
